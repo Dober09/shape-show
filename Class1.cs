fd1fa4ee3512397c3d2cf3e7737fbe1e2a629378
Dober09 <lebohanglehutjo@gmail.com>
2024-06-07 13:45:05 +0200

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// namespace ConsoleApp4
+// {
+//     public abstract class Area
+//     {
+
+//         public double getArea(int a)
+//         {
+//             return 0;
+//         }
+
+//     }
+
+//     public class Tripezium: Area
+//     {
+
+//         public double getArea(double a, double b, double h)
+//         {
+
+//             return 0.5 * (a + b) * h;
+
+//         }
+
+//     }
+
+//     public class Square : Area
+//     {
+
+//         public double getArea(double s)
+//         {
+
+//             return s*s;
+
+//         }
+
+//     }
+
+//     public class Triangle : Area
+//     {
+
+//         public double getArea(double b, double h)
+//         {
+
+//             return 0.5 * b * h;
+
+//         }
+
+//     }
+
+//     public class Circle : Area
+//     {
+//         double pi = Math.PI;
+//         public double getArea(double r)
+//         {
+
+//             return pi * r*r;
+
+//         }
+
+//     }
+
+//     public class Rectangle : Area
+//     {
+//         public double getArea(double l, double w)
+//         {
+
+//             return  l * w;
+
+//         }
+
+//     }
+
+// }
+
+namespace ConsoleApp4
+{
+    public abstract class ShapeArea{
+        public abstract double CircleArea(double radius);
+        public abstract double SquareArea(double side);
+        public abstract double TriangleArea(double height, double bases);
+
+        public abstract double TrapeziodArea(double a_base,double b_base, double height);
+
+    }
+    public class Shape:ShapeArea{
+
+        private const double PI = Math.PI;
+        private double radius;
+        private double height;
+        private double bases;
+        private double a_base;
+        private double b_base;
+
+        private double GetRadius() { return radius; }
+        private double GetHeight() { return height; }
+        private double GetBases() { return bases; }
+        private double GetA_base() { return a_base; }
+
+         private double GetB_base() { return b_base; }
+
+
+         public void SetRadius(double radius) { this.radius = radius;}
+         public void SetHeight(double height) { this.height = height;}
+         public void SetBases(double bases) { this.bases = bases;}
+         public void SetA_base(double a_base) { this.a_base = a_base;}
+         public void SetB_base(double b_base) { this.b_base = b_base;}
+
+        public override double CircleArea(double radius)
+        {
+            return 2*PI * Math.Pow(radius,2);
+        }
+
+        public override double SquareArea(double side)
+        {
+            return Math.Pow(side,2);
+        }
+
+        public override double TriangleArea(double height, double bases)
+        {
+            return 0.5 * (height*bases);
+        }
+
+        public override double TrapeziodArea(double a_base, double b_base, double height)
+        {
+            return 0.5 *(a_base + b_base)*height;
+        }
+
+        public void Result(int choice){
+            switch (choice){
+                case 1:
+                    Console.WriteLine("Trapeziod Area : {0}",TrapeziodArea(GetA_base(), GetB_base(), GetHeight()));
+                    break;
+                case 2:
+                    Console.WriteLine("Square Area : {0}",SquareArea(GetRadius()));
+                    break;
+                case 3:
+                    Console.WriteLine("Triangle Area : {0}",TriangleArea(GetHeight(),GetBases()));
+                    break;
+                case 4:
+                    Console.WriteLine("Circle Area : {0}",CircleArea(GetRadius()));
+                    break;
+                default:
+                    System.Console.WriteLine("Invalid Choice");
+                    break;
+               
+
+
+            }
+           
+        }
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    { 
+
+    
+        public static void Main(string[] args) 
+        {
+
+            bool looper = true;
+            Shape shape =  new Shape();
+            
+            Console.WriteLine("Please select a shape \n 1. Tripezium \n 2. Square \n 3. Triangle \n 4. Circle \n 5. Rectangle");
+            int input = int.Parse(Console.ReadLine());
+      
+           
+
+            do
+            {
+                switch (input)
+                {
+                    case 1:
+                         Console.WriteLine("Please enter the tripezium A length value : ");
+                         shape.SetA_base(double.Parse(Console.ReadLine()));
+                         Console.WriteLine("Please enter the tripezium B length value : ");
+                         shape.SetB_base(double.Parse(Console.ReadLine()));
+                         Console.WriteLine("Please enter the tripezium breadth value  : ");
+                         shape.SetBases(double.Parse(Console.ReadLine()));
+                         Console.WriteLine("Please enter the tripezium height value   : ");
+                         shape.SetHeight(double.Parse(Console.ReadLine()));
+
+                        
+                         
+                         break;
+                    case 2:
+                         Console.WriteLine("Please enter the square side value : ");
+                         shape.SetRadius(double.Parse(Console.ReadLine()));
+                        break;
+                         
+
+
+
+                    default:
+                    break;
+                }
+
+
+
+                // if (shape == 1)
+                // {
+
+                //     double a = 0, b = 0, h = 0;
+
+                //     Console.WriteLine("Please enter the tripezium length value");
+                //     a = double.Parse(Console.ReadLine());
+                //     b = double.Parse(Console.ReadLine());
+                //     Console.WriteLine("Please enter the tripezium height value");
+                //     h = double.Parse(Console.ReadLine());
+
+                //     Tripezium tripezium = new Tripezium();
+
+                //     Console.WriteLine("The Area is: " + tripezium.getArea(a, b, h));
+                //     break;
+                // }
+
+                // else if (shape == 2)
+                // {
+                //     Console.WriteLine("Please enter the square side value");
+                //     double s = double.Parse(Console.ReadLine());
+
+                //     Square square = new Square();
+
+                //     Console.WriteLine("The Area is: " + square.getArea(s));
+                //     break;
+                // }
+
+                // else if (shape == 3)
+                // {
+                //     Console.WriteLine("Please enter the triangle base value");
+                //     double b = double.Parse(Console.ReadLine());
+                //     Console.WriteLine("Please enter the triangle height value");
+                //     double h = double.Parse(Console.ReadLine());
+
+                //     Triangle triangle = new Triangle();
+
+                //     Console.WriteLine("The Area is: " + triangle.getArea(b, h));
+                //     break;
+                // }
+
+                // else if (shape == 4)
+                // {
+                //     Console.WriteLine("Please enter the circle radius value");
+                //     double r = double.Parse(Console.ReadLine());
+            
+             
+
+                //     Circle circle = new Circle();
+
+                //     Console.WriteLine("The Area is: " + circle.getArea(r));
+                //     break;
+                // }
+
+                // else if (shape == 5)
+                // {
+                //     Console.WriteLine("Please enter the rectangle length value");
+                //     double l = double.Parse(Console.ReadLine());
+                //     Console.WriteLine("Please enter the rectangle width value");
+                //     double w = double.Parse(Console.ReadLine());
+
+
+
+                //     Rectangle rectangle = new Rectangle();
+
+                //     Console.WriteLine("The Area is: " + rectangle.getArea(l, w));
+                //     break;
+                // }
+
+                // else 
+                // {
+
+                //     Console.WriteLine("Invalid selection \n Please select a correct shape: \n 1. Tripezium \n 2. Square \n 3. Triangle \n 4. Circle \n 5. Rectangle");
+                //     shape = int.Parse(Console.ReadLine());
+                //     looper = false;
+
+                // }
+
+            }while (looper);
+        }
+    
+    }
+
+}
